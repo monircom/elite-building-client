a72b464cc6a02571cdd601fe32304d23f963c88d
monircom <monircom@gmail.com>
2024-06-11 19:28:52 +0600

    Dashbar Added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,8 @@ import ErrorPage from "../pages/ErrorPage";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import RoomDetails from "../pages/RoomDetails/RoomDetails";
+import DashboardLayout from '../layouts/DashboardLayout'
+import Statistics from '../pages/Dashboard/Common/Statistics'
 
 export const router = createBrowserRouter([
   {
@@ -18,13 +20,87 @@ export const router = createBrowserRouter([
       },
       { path: "/login",
        element: <Login /> },
-       
+
       { path: "/signup",
        element: <SignUp /> },
       {
         path: "/room/:id",
         element: <RoomDetails />,
       },
+    ],
+  },
+  {
+    path: '/dashboard',
+    element: (
+     
+        <DashboardLayout />
+      
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          
+            <Statistics />
+          
+        ),
+      },
+      // {
+      //   path: 'add-room',
+      //   element: (
+      //     <PrivateRoute>
+      //       <HostRoute>
+      //         <AddRoom />
+      //       </HostRoute>
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'my-listings',
+      //   element: (
+      //     <PrivateRoute>
+      //       <HostRoute>
+      //         <MyListings />
+      //       </HostRoute>
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'manage-users',
+      //   element: (
+      //     <PrivateRoute>
+      //       <AdminRoute>
+      //         <ManageUsers />
+      //       </AdminRoute>
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'my-bookings',
+      //   element: (
+      //     <PrivateRoute>
+      //       <MyBookings />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'manage-bookings',
+      //   element: (
+      //     <PrivateRoute>
+      //       <HostRoute>
+      //         <ManageBookings />
+      //       </HostRoute>
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'profile',
+      //   element: (
+      //     <PrivateRoute>
+      //       <Profile />
+      //     </PrivateRoute>
+      //   ),
+      // },
     ],
   },
 ]);
