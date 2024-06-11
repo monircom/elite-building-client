fd09027e3c7cb80e668a775c47c609a501819fbe
monircom <monircom@gmail.com>
2024-06-11 18:20:42 +0600

    slider added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 //import Categories from '../../components/Categories/Categories'
 import Rooms from '../../components/Home/Rooms'
+import Carousel from '../../components/Carousel'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       {/* Categories section  */}
       {/* <Categories /> */}
       {/* Rooms section */}
+      <Carousel></Carousel>
       <Rooms />
     </div>
   )
